1b77889701048d596aa8457a4c4461974ae95b7f
Imran Ahmed <devimranahmedbmj@gmail.com>
2024-03-13 07:23:40 +0500

    Add Javascript Project

---
+++ app.js
@@ -0,0 +1,255 @@
+// Assignment #16 JavaScript
+
+// Chapter # 01 Alerts
+
+// Question No:01
+
+// alert("Thanks for your input!");
+
+
+
+
+// Chapter # 2 Variables for Strings
+
+// Question No:01
+
+
+
+// var Myname = "Mark Henrry";
+
+// console.log(Myname)
+
+
+// Question No:02
+
+// var firstName="mark"
+// var secondName="ace"
+// console.log(firstName + " " + secondName)
+
+
+
+// Question No:02
+
+// var firstName="mark"
+// var secondName="ace"
+// alert(firstName )
+// alert(secondName )
+
+// Question No:02
+
+// var nationality;
+// nationality = "U.S.";
+// alert(nationality)
+
+
+
+// Chapter # 3 Variables for Numbers
+
+
+// Question No:01
+
+// var weight = 150;
+
+// console.log(weight)
+
+
+
+// Question No:02
+
+// var num1=100;
+// var num2=200;
+
+// console.log(num1+num2)
+// console.log(num1-num2)
+// console.log(num1*num2)
+// console.log(num1/num2)
+
+
+// Question No:03
+
+// var num1=100;
+// var num2=5;
+// console.log(num1**num2)
+
+// Question No:04
+
+
+//  var originalNumber = 123;
+//  var newNumber = 7;
+// console.log(originalNumber + newNumber)
+
+
+
+
+// Chapter # 4 Variable Names Legal and Illegal
+
+// Ilegal variables
+
+// var var='Wrongvariable';
+
+// var 123num=123;
+
+// var &Number=1233
+
+
+
+// Legal Variables
+// var myVar="legal Variable"
+// var myNumber=300
+// userResponse
+// userResponseTime
+// userResponseTimeLimit
+
+
+
+// Chapter # 5 Math expressions:
+            // Familiar operators
+
+
+// Question No:01
+
+            // var popularNumber = 2 + 2;
+
+//   alert(popularNumber)
+
+// Question No:02
+
+//    var num = 10;
+//    var popularNumber = num + 200;
+//    alert(popularNumber)
+
+
+
+  // Question No:03
+
+//   var popularNumber = 100 - 2;
+
+//   alert(popularNumber)
+
+
+  // Question No:04
+
+//   var popularNumber = 100 * 2;
+
+//   alert(popularNumber)
+
+  // Question No:05
+
+//   var popularNumber = 100 / 2;
+
+//   alert(popularNumber)
+
+
+
+
+
+
+
+// Chapter # 6 Math expressions:
+            // UnFamiliar operators
+
+// Post Addition Method
+  // Question No:01
+
+//    var num = 1;
+//    var newNum = num++;
+// console.log(newNum)
+// Post Substraction Method
+
+// Question No:02
+
+// var num = 2;
+// var newNum = num--;
+// console.log(newNum)
+
+
+// Pre Addition Method
+  // Question No:03
+
+//    var num = 3;
+//    var newNum = ++num;
+// console.log(newNum)
+
+// Post Substraction Method
+
+// Question No:04
+
+// var num = 10;
+// var newNum = --num;
+// console.log(newNum)
+
+
+
+
+
+// Chapter # 7 Math expressions:
+// Eliminating ambiguity
+
+
+// Question No:01
+// var totalCost = (2 + 4) * 8;
+// console.log(totalCost)
+
+
+// Question No:02
+// var resultOfComputation = (4 * 8) * 6 + 6;
+
+// console.log(resultOfComputation)
+
+
+// Chapter # 8 Concatenating text strings
+  
+// Question No:01
+
+// var message = "Thanks for your input!";
+// var userName="Imran Ahmed"
+//  alert(message);
+// alert("Thanks," + userName + "!");
+
+
+// Question No:02
+//  var message = "Thanks For Coming, ";
+// var userName="Imran Ahmed"
+// var banger = "!";
+//  alert(message + userName + banger);
+
+
+
+// Chapter 9 Prompts
+
+// Question : 01
+// alert("This is my alert")
+
+
+// Question : 02
+
+//   var question = "Your Name?";
+//   var defaultAnswer = "My Name is Imran Ahmed";
+//   var spec = prompt(question, defaultAnswer);
+
+
+
+
+
+
+// Chapter 10 if statements
+
+// Question : 01
+//  var x = prompt("Where You Live?");
+//  if (x === "pakistan") {
+//  alert("Correct!");
+
+ }
+
+ // Question : 02
+//  var x = prompt("how Old Are You?");
+//  if (x === 18) {
+//  alert("You are Elgible For Job");
+
+ }
+
+
+// Chapter 11 Comparison operators
+//    Question : 01
+
+
